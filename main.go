46a084207ba20dcea32403fce0a08829a10a0fe7
Vikas <vikas3517@gmail.com>
2023-09-28 18:46:46 +0530

    Database added to project

--- main.go
+++ main.go
@@ -8,15 +8,25 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql","root:@tcp(localhost:3306)/blog_post")
+	db, err := sql.Open("mysql","root:@tcp(localhost:3306)/go_blog")
 	if err != nil {
 		fmt.Println("Error", err)
+		return
 	}
 	defer db.Close()
 
 	data, err := db.Query("select * from blog")
 	if err != nil {
 		fmt.Println("Error",err)
+		return
 	}
-	fmt.Println(data)
+	for data.Next() {
+		var post Post
+		err = data.Scan(&post.Id, &post.Title, &post.Date, &post.Author, &post.Content)
+		if err != nil {
+			fmt.Println("Error : ",err)
+			return
+		}
+		fmt.Println(post)
+	}
 }

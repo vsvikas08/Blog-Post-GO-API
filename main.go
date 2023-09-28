efc77b8118cd1fc95f2db25b9e162c174bb39a5a
Vikas <vikas3517@gmail.com>
2023-09-28 19:37:42 +0530

    Get Post by id created

--- main.go
+++ main.go
@@ -25,7 +25,23 @@ func GetAllPost(c *gin.Context) {
 		}
 		posts = append(posts, post)
 	}
-	c.JSON(http.StatusAccepted, gin.H{"data": posts})
+	c.JSON(http.StatusOK, gin.H{"data": posts})
+}
+
+func GetPostByID(c *gin.Context) {
+	db := getDBInstance().db
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "ID can not be empty"})
+		return
+	}
+	var post Post
+	err := db.QueryRow("SELECT * FROM blog WHERE post_id = ?",id).Scan(&post.Id,&post.Date,&post.Author,&post.Title,&post.Content)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message" : "Post doesn't exist."})
+		return
+	}
+	c.JSON(http.StatusAccepted, gin.H{"data" : post})
 }
 func main() {
 	db := getDBInstance().db
@@ -34,6 +50,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/post", GetAllPost)
+	r.GET("/post/:id", GetPostByID)
 
 	r.Run(":8000")
 }
